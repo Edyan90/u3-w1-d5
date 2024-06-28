22b9b37e73bdc70c5e8a69f93894aded06e40f75
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-06-28 11:12:18 +0200

    top bar e footer ricreati

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Components/Footer";
+import TopBar from "./Components/TopBar";
 function App() {
-  return <Footer />;
+  return (
+    <>
+      <TopBar />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
